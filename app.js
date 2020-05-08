4c17d094acb3bb8f5eb3316cc75b3cff1591310a
Ravi Upadhyay <raviupadhyay.bkn@gmail.com>
2020-05-08 23:52:22 +0200

    Chapter 2 - EventModule and EventEmitter Class

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
  
-const math = require('./math');
-console.log('Hurray, Nodejs is up and running'); 
-console.log(math);
-console.log(math.sum(1,2));
+const Event = require('events');
+
+const eventManager = new Event;
+
+//Simple working with events
+eventManager.on('shout', () => {
+    console.log('Why you are shouting, I am not deaf');
+});
+
+eventManager.emit('shout');
+
+//Extending class
+class ErrorHandler extends Event {
+    constructor() {
+        super();
+    }
+
+    get errorCodes() {
+        return {
+            700: 'Age out of the bounds',
+            701: 'Age out of the bounds',
+            701: 'Income details do not match',
+        }
+    }
+}
+
+const errors = new ErrorHandler;
+errors.on('700', () => {
+    console.log(errors.errorCodes[700]);
+});
+errors.on('701', () => {
+    console.log(errors.errorCodes[701]);
+});
+
+errors.emit('700');
+errors.emit('701');
