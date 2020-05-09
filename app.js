4d14f4af294b28e093557997218e149c8ff395df
Ravi Upadhyay <raviupadhyay.bkn@gmail.com>
2020-05-09 15:56:14 +0200

    Chapter 6 - Working with fs module, Read & Write to the files

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-/* Chapter 5 - File system module, working with directories */
-/* 5.1. make and remove directories */
-/* 5.2. empty directories before deleting */
+/* Chapter 6 - Read and Write the Streams  */
+/* 6.1. ReadStream and WriteStream */
 const fs = require('fs');
+let counter = 1;
+const readStream = fs.createReadStream('./file-to-read.txt', 'utf8');
+const writeStream = fs.createWriteStream('./file-to-write.txt');
 
-fs.mkdir('node-experiment', (err) => {
-    if(err) console.log(err);
-    else {
-        console.log('Directory Created Sucessfully');
-        
-        fs.writeFile('./node-experiment/experiment-file.txt', 'This is an experiment', (err) => {
-            if(err) console.log(err);
-            else {
-                console.log('file has been created sucessfully, ./node-experiment/experiment-file.txt');
+readStream.on('data', (stream)=> {
+    console.log(`readStream has read stream: ${counter}: `, stream);
+    counter++;
+    writeStream.write(stream);
+});
 
-                fs.readdir('./node-experiment', (err, files) => {
-                    if (err) console.log(err);
-                    else {
-                        console.log('./node-experiment contains files: ', files);
-                        if(files.length) {
-                            for(let file of files) {
-                                fs.unlink(`./node-experiment/${file}`, (err) => {
-                                    if(err) console.log(err);
-                                    else console.log(`file deleted form ./node-experiment: ${file}`);
-                                });
-                            }
-                        }
-                        fs.rmdir('node-experiment', (err) => {
-                            if(err) console.log(err);
-                            else console.log('Directory Deleted Successfully');
-                        });
-                    }
-                });
-            }
-        });
+/* 6.2. ReadFile without stream */
+fs.readFile('./file-to-read.txt', (err, file)=> {
+    console.log('readFile method: ');
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(file);
     }
 });
